aee476639c389bf2c933c17033e736c73b15a734
Rajanish Kumar Prajapati <76473129+Rajanish-45464@users.noreply.github.com>
2023-12-06 23:14:23 +0530

    Add files via upload
    This is Advance level of Star Pattern program in java.

---
+++ AdvanceStarPattern1.java
@@ -0,0 +1,47 @@
+
+
+/*
+	**
+	***
+	****
+	*****
+	******
+	*******
+	********
+	*********
+	********
+	*******
+	******
+	*****
+	****
+	***
+	**
+	*
+
+*/
+package Advance_StarPackage;
+
+public class AdvanceStarPattern1 {
+
+	public static void main(String[] args) {
+	
+		for(int i=0;i<9;i++)
+		{
+			for(int j=0;j<i+1;j++)
+			{
+				System.out.print("*");		
+			}
+			
+			System.out.println();
+		}
+		for(int i=0;i<9;i++)
+		{
+			for(int j=0;j<9-i-1;j++)
+			{
+				System.out.print("*");		
+			}
+			
+			System.out.println();
+		}
+	}
+}
